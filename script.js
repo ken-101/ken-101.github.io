bd3308b6e8bbcbca334c403265ff27f491ad934a
ken-101 <111231705+ken-101@users.noreply.github.com>
2022-12-24 00:29:45 +0800

    added some js

--- script.js
+++ script.js
@@ -6,29 +6,44 @@ const button = document.querySelector('#button');
 const div = document.querySelector('#div');
 
 
-const menuBtn = document.querySelector('.menu-btn');
+const menubutton = document.querySelector('.menu-btn');
 const menu = document.querySelector('.burger-menu');
 const dropdown = document.querySelector('.top-right');
+const header = document.querySelector('#header');
+const nav = document.querySelector('#top-bar')
 
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
+ menubutton.addEventListener('click', () => {
+   menu.classList.toggle('open');
+ });
 const a = 'click'
-// menuBtn.addEventListener('click', () => {
-//   dropdown.classList.toggle('slide');
-// });
-// menuBtn.addEventListener('click', () => {
-//   dropdown.classList.toggle('top-right');
-// });
-const menuBtna = document.querySelector('#menuBtn');
-const openBtn = document.querySelector('.open');
-const closeBtn = document.querySelector('.close');
-
-
-document.addEventListener('click', event => {
-  if (event.target.closest(openBtn)) {
-    menuBtna.classList.add('slide');
-  } else if (event.target.closest(closeBtn)) {
-    menuBtna.classList.remove('slide');
-  }
+menubutton.addEventListener('click', () => {
+  dropdown.classList.toggle('slide');
 });
+
+menubutton.addEventListener('click', () => {
+  if (dropdown.classList.contains('top-right')) {
+  dropdown.classList.remove('top-right');
+  } else {
+  dropdown.classList.add('top-right');
+  }
+  });
+// for bug fix do not touch this function
+header.addEventListener('click', () => {
+  nav.classList.toggle('top-nav');
+});
+// // menuBtn.addEventListener('click', () => {
+// //   dropdown.classList.toggle('top-right');
+// // });
+// const menuBtna = document.querySelector('#menuBtn');
+// const openBtn = document.querySelector('.open');
+// const closeBtn = document.querySelector('.close');
+
+
+// document.addEventListener('click', event => {
+//   if (event.target.closest(openBtn)) {
+//     menuBtna.classList.add('slide');
+//   } else if (event.target.closest(closeBtn)) {
+//     menuBtna.classList.remove('slide');
+//   }
+// });
+
