459c9026762985ca8c058e92589a5648840aff2e
Vinnity <spdsheart@gmail.com>
2023-01-08 22:46:05 +0800

    a

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const burgerMenuButton = document.getElementById('burger-menu-button');
-const navigationMenu = document.querySelector('nav ul');
+// const burgerMenuButton = document.getElementById('burger-menu-button');
+// const navigationMenu = document.querySelector('nav ul');
 
 
-const button = document.querySelector('#button');
-const div = document.querySelector('#div');
+// const button = document.querySelector('#button');
+// const div = document.querySelector('#div');
 
 
-const menubutton = document.querySelector('.menu-btn');
-const menu = document.querySelector('.burger-menu');
-const dropdown = document.querySelector('.top-right');
-const header = document.querySelector('#header');
-const nav = document.querySelector('#top-bar')
+// const menubutton = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.burger-menu');
+// const dropdown = document.querySelector('.top-right');
+// const header = document.querySelector('#header');
+// const nav = document.querySelector('#top-bar')
 
- menubutton.addEventListener('click', () => {
-   menu.classList.toggle('open');
- });
-const a = 'click'
-menubutton.addEventListener('click', () => {
-  dropdown.classList.toggle('slide');
-});
+//  menubutton.addEventListener('click', () => {
+//    menu.classList.toggle('open');
+//  });
+// const a = 'click'
+// menubutton.addEventListener('click', () => {
+//   dropdown.classList.toggle('slide');
+// });
 
-menubutton.addEventListener('click', () => {
-  if (dropdown.classList.contains('top-right')) {
-  dropdown.classList.remove('top-right');
-  } else {
-  dropdown.classList.add('top-right');
-  }
-  });
-// for bug fix do not touch this function
-header.addEventListener('click', () => {
-  nav.classList.toggle('top-nav');
-});
+// menubutton.addEventListener('click', () => {
+//   if (dropdown.classList.contains('top-right')) {
+//   dropdown.classList.remove('top-right');
+//   } else {
+//   dropdown.classList.add('top-right');
+//   }
+//   });
+// // for bug fix do not touch this function
+// header.addEventListener('click', () => {
+//   nav.classList.toggle('top-nav');
+//});
 // // menuBtn.addEventListener('click', () => {
 // //   dropdown.classList.toggle('top-right');
 // // });
@@ -47,3 +47,10 @@ header.addEventListener('click', () => {
 //   }
 // });
 
+const toggle = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
